59fd78f840fd1bc2ac6957192d44bd80c8b6f1d3
qwertyama <yamashita.s.al@m.titech.ac.jp>
2021-08-30 18:36:22 +0900

    first commit UnionFind.cpp

---
+++ UnionFind.cpp
@@ -0,0 +1,58 @@
+struct UnionFind {
+private:
+	int N;
+	int tree_cnt_data;
+	std::vector<int> val;
+
+	int root(int x) {
+		if(val[x] < 0) {
+			return x;
+		}
+		else {
+			return val[x] = root(val[x]);
+		}
+	}
+
+public:
+	UnionFind(int N_input) {
+		N = N_input;
+		tree_cnt_data = N_input;
+		val = std::vector<int> (N_input, -1);
+	}
+
+	void unite(int x, int y) {
+		x = root(x);
+		y = root(y);
+		if(x == y) {
+			return;
+		}
+		if(val[x] > val[y]) {
+			std::swap(x, y);
+		}
+		val[x] += val[y];
+		val[y] = x;
+		tree_cnt_data--;
+		return;
+	}
+
+	bool same(int x, int y) {
+		if(root(x) == root(y)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	int tree_cnt() {
+		return tree_cnt_data;
+	}
+
+	int tree_size(int x) {
+		return -val[root(x)];
+	}
+
+	bool is_root(int x) {
+		return (val[x] < 0 ? true : false);
+	}
+};
