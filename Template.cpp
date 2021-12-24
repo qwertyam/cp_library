40436724b2270140093961164ab3e1ae580fa3a0
qwertyama <yamashita.s.al@m.titech.ac.jp>
2021-12-24 14:06:15 +0900

    modified

--- Template.cpp
+++ Template.cpp
@@ -1,10 +1,18 @@
+
 #include <bits/stdc++.h>
 
-using namespace std;
+
+namespace qwertyam {
+	void main() {
+
+	}
+} // namespace qwertyam
+
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	qwertyam::main();
 	return 0;
 }
 
