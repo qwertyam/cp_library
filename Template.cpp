d94f219efd27081e5cc58b3001feb8976af4f466
qwertyama <yamashita.s.al@m.titech.ac.jp>
2022-04-08 23:00:08 +0900

    delete gcd lcm pwmd

--- Template.cpp
+++ Template.cpp
@@ -1,24 +1,19 @@
 
-
 #include <bits/stdc++.h>
 
 namespace qwertyam {
-
 int d4r[] = {0, 1, 0, -1};
 int d4c[] = {1, 0, -1, 0};
 int d8r[] = {1, 1, 0, -1, -1, -1, 0, 1};
 int d8c[] = {0, 1, 1, 1, 0, -1, -1, -1};
 constexpr int MOD = 1000000007;
 // constexpr int MOD = 998244353;
+// using mint = ModInt<MOD>;
 constexpr int INF = 1001001001;
 constexpr long long LINF = 1001002003004005006;
 constexpr char endl = '\n';
-template <typename T> void chmax(T& a, const T& b);
-template <typename T> void chmin(T& a, const T& b);
-long long pwmd(const long long& a, const long long& n);
-long long gcd(const long long& a, const long long& b);
-long long lcm(const long long& a, const long long& b);
-
+template <typename T> void chmax(T& a, const T& b) {a = std::max(a, b);} 
+template <typename T> void chmin(T& a, const T& b) {a = std::min(a, b);}
 
 struct MAIN {
 	void main() {
@@ -41,38 +36,4 @@ int main() {
 	return 0;
 }
 
-// -----------------------------------------------
-
-namespace qwertyam {
-
-template <typename T> 
-void chmax(T& a, const T& b) {
-	a = std::max(a, b);
-	return;
-}
-
-template <typename T>
-void chmin(T& a, const T& b) {
-	a = std::min(a, b);
-	return;
-}
-
-long long pwmd(const long long& a, const long long& n) {
-	if (n == 0) return 1LL;
-	long long tmp = pwmd(a, n / 2);
-	tmp = tmp * tmp % MOD;
-	if (n % 2 == 1) tmp = tmp * a % MOD;
-	return tmp;
-}
-
-long long gcd(const long long& a, const long long& b) {
-	if (b == 0) return a;
-	else return gcd(b, a % b);
-}
-
-long long lcm(const long long& a, const long long& b) {
-	return a / gcd(a, b) * b;
-}
-
-} // namespace qwertyam
 
